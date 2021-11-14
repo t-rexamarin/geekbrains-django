14aa557c3830048387ef5302d1c1ca4bed93758e
Roma <65kgm9ca@gmail.com>
2021-11-14 17:01:32 +0300

    dynamic content for index.html

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from .models import Products
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'title': 'GeekShop from view',
+        'header': 'GeekShop Store',
+        'header_info': 'Новые образы и лучшие бренды на GeekShop Store. '
+                  'Бесплатная доставка по всему миру! Аутлет: до -70% Собственный бренд. -20% новым покупателям.',
+        'user': 'User from view'
+    }
+
+    return render(request, 'index.html', context)
 
 
 def products(request):
