68010b62fd8343e86cd0cde98e2da04fbe5f11ef
Roma <65kgm9ca@gmail.com>
2021-11-14 17:03:06 +0300

    create model for Products

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Products(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=128)
+    description = models.TextField(verbose_name='Описание', blank=True)
+    price = models.DecimalField(verbose_name='Цена', max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name, self.description, self.price
