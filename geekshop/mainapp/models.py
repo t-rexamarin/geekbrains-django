68782b1acec81953f1d08ed441d0e97102d27e3b
Roma <65kgm9ca@gmail.com>
2021-11-15 12:11:37 +0300

    add img_name attribute to model Product and fixture

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -6,5 +6,6 @@ class Products(models.Model):
     name = models.CharField(verbose_name='Название', max_length=128)
     description = models.TextField(verbose_name='Описание', blank=True)
     price = models.DecimalField(verbose_name='Цена', max_digits=8, decimal_places=2)
+    img_name = models.TextField(verbose_name='Название картинки', blank=True)
 
     objects = models.Manager()
