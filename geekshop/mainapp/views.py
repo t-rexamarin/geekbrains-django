c5d8823319d876b12f11d30e3199e7f926067fde
Roma <65kgm9ca@gmail.com>
2021-11-15 09:02:39 +0300

    add title in index, products controllers

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -5,17 +5,21 @@ from .models import Products
 # Create your views here.
 def index(request):
     context = {
-        'title': 'GeekShop from view',
+        'title': 'GeekShop',
         'header': 'GeekShop Store',
         'header_info': 'Новые образы и лучшие бренды на GeekShop Store. '
                   'Бесплатная доставка по всему миру! Аутлет: до -70% Собственный бренд. -20% новым покупателям.',
         'user': 'User from view'
     }
 
-    return render(request, 'index.html', context)
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
     products = Products.objects.all()
-    context = {'Products': products}
-    return render(request, 'products.html', context)
+    context = {
+        'title': 'GeekShop - Каталог',
+        'products': products
+    }
+
+    return render(request, 'mainapp/products.html', context)
