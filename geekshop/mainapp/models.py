5852ba3f356c2525656b611312cff3999077c485
Roma <65kgm9ca@gmail.com>
2021-11-18 19:40:39 +0300

    models update

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -2,10 +2,25 @@ from django.db import models
 
 
 # Create your models here.
-class Products(models.Model):
-    name = models.CharField(verbose_name='Название', max_length=128)
-    description = models.TextField(verbose_name='Описание', blank=True)
-    price = models.DecimalField(verbose_name='Цена', max_digits=8, decimal_places=2)
-    img_name = models.TextField(verbose_name='Название картинки', blank=True)
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+    created_at = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='дата изменения', auto_now=True)
 
-    objects = models.Manager()
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='имя продукта', max_length=128)
+    image = models.ImageField(upload_to='products_image', blank=True)
+    short_desc = models.CharField(verbose_name='краткое описание продукта', max_length=60, blank=True)
+    price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
+    created_at = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='дата изменения', auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name})"
