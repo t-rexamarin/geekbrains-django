561afa0787447edcba8de50594375ce8ced6bd58
Roma <65kgm9ca@gmail.com>
2021-11-19 12:22:21 +0300

    small fixes

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
