e70d47279977cfff477b232edfd26103cc9eef45
Roma <65kgm9ca@gmail.com>
2021-11-19 17:48:58 +0300

    models update

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -18,7 +18,7 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     name = models.CharField(verbose_name='имя продукта', max_length=128)
-    image = models.ImageField(upload_to='products_image', blank=True)
+    image = models.ImageField(upload_to='product_image', blank=True)
     short_desc = models.CharField(verbose_name='краткое описание продукта', max_length=60, blank=True)
     price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
