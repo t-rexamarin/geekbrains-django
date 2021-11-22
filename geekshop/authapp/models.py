ff9a1150a32ff1bddbb27a4de4c652a3075913c7
Roma <65kgm9ca@gmail.com>
2021-11-22 18:30:19 +0300

    add date fields

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.timezone import now
 
 
 # Create your models here.
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_image', blank=True)
     age = models.PositiveIntegerField(default=18)
+    # created_at = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
+    # если указывать now(), а не now, то при создании миграции будет высвечивать ворнинг Fixed default value provided
+    created_at = models.DateTimeField(verbose_name='дата создания', default=now)
+    updated_at = models.DateTimeField(verbose_name='дата изменения', auto_now=True)
