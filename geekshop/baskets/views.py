a0a3c5ac191ff3fb65021b1c274841a7e29b718f
Roma <65kgm9ca@gmail.com>
2021-11-28 20:51:35 +0300

    add user basket ajax

--- geekshop/baskets/views.py
+++ geekshop/baskets/views.py
@@ -1,10 +1,14 @@
-from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
+
 from baskets.models import Basket
 from mainapp.models import Product
 
 
 # Create your views here.
+@login_required
 def basket_add(request, id):
     user_select = request.user
     product = Product.objects.get(id=id)
@@ -20,6 +24,25 @@ def basket_add(request, id):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, basket_id):
     Basket.objects.get(id=basket_id).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_edit(request, id_basket, quantity):
+    if request.is_ajax():
+        basket = Basket.objects.get(id=id_basket)
+
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+
+        baskets = Basket.objects.filter(user=request.user)
+        context = {'baskets': baskets}
+        result = render_to_string('baskets/basket.html', context)
+
+        return JsonResponse({'result': result})
