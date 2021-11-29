6e9f51674585f0291ed5ccadac2a1ef8bd24a9e8
Roma <65kgm9ca@gmail.com>
2021-11-29 20:13:45 +0300

    add products ajax

--- geekshop/static/js/basket.js
+++ geekshop/static/js/basket.js
@@ -14,4 +14,20 @@ window.onload = function() {
             });
         event.preventDefault()
     })
+
+        $('.card_add_basket').on('click', 'button[type="button"]', function () {
+        let product_id = event.target.value
+        let product_name = event.target.getAttribute('prod_name')
+
+        $.ajax(
+            {
+                url: "/baskets/add/" + product_id + "/",
+                success: function (data){
+                    $('.card_add_basket').html(data.result)
+                    alert(`Товар ${product_name} добавлен в корзину`)
+                },
+//                errors: function()
+            });
+        event.preventDefault()
+    })
 }
