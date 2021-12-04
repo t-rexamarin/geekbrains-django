2cd0bf565f63650f721352f177b835780a3cf5a0
Roma <65kgm9ca@gmail.com>
2021-12-04 12:29:11 +0300

    users update controller

--- geekshop/adminapp/forms.py
+++ geekshop/adminapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from authapp.forms import UserRegistrationForm
+from authapp.forms import UserRegistrationForm, UserChangeProfileForm
 from authapp.models import User
 
 
@@ -14,4 +14,22 @@ class UserAdminRegisterForm(UserRegistrationForm):
 
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
+
+        self.fields['image'].widget.attrs['class'] = 'custom-file-input'
+
+
+class UserAdminProfileForm(UserChangeProfileForm):
+    # email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control py-4', 'readonly': False}))
+    # username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control py-4', 'readonly': False}))
+    email = forms.EmailField(widget=forms.EmailInput())
+    username = forms.CharField(widget=forms.TextInput())
+
+    def __init__(self, *args, **kwargs):
+        super(UserAdminProfileForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs['readonly'] = False
+        self.fields['username'].widget.attrs['readonly'] = False
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
         self.fields['image'].widget.attrs['class'] = 'custom-file-input'
--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 
-from adminapp.forms import UserAdminRegisterForm
+from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 from authapp.models import User
 
 
@@ -38,8 +38,24 @@ def admin_users_create(request):
     return render(request, 'adminapp/admin-users-create.html', context)
 
 
-def admin_users_update(request):
-    return render(request, 'adminapp/admin-users-update-delete.html')
+def admin_users_update(request, pk):
+    user_select = User.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, instance=user_select, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('adminapp:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=user_select)
+
+    context = {
+        'title': 'Geekshop - Админ | Редактирование',
+        'form': form,
+        'user_select': user_select
+    }
+
+    return render(request, 'adminapp/admin-users-update-delete.html', context)
 
 
 def admin_users_delete(request):
