82c962b67b9270cf41dda9e8dc615fd62334f363
Roma <65kgm9ca@gmail.com>
2021-12-04 15:57:55 +0300

    admin custom table

--- geekshop/mainapp/admin.py
+++ geekshop/mainapp/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Product, ProductCategory
 
 # Register your models here.
-admin.site.register(Product)
 admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'short_desc', ('price', 'quantity'), 'category')
+    readonly_fields = ('short_desc',)
+    ordering = ('name', 'price',)
+    search_fields = ('name',)
