e7671ee67d1a55514da7a83365b16914001ba8b4
Roma <65kgm9ca@gmail.com>
2021-12-11 15:58:19 +0300

    - move navbar to includes to make it easier to rerender while products filtered by category - refactor products view to for ajax

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -17,9 +17,18 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request, page=1):
-    productsCategories = ProductCategory.objects.all()
-    products = Product.objects.all()
+# TODO:
+# получается запутанная ситуация при фильтрации категорий и навигационной панелью пагинации
+# т.к. если отфильтровать по категории, а потом нажать на страницу пагинации в панели
+# то вернет на страницу пагинации всех товаров, без учета категории
+# как то определять, что мы находимся на отфильтрованной категории?
+def products(request, category_id=None, page=1):
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+
+    products_categories = ProductCategory.objects.all()
     paginator = Paginator(products, per_page=3)
 
     try:
@@ -31,11 +40,15 @@ def products(request, page=1):
 
     context = {
         'title': 'GeekShop | Каталог',
-        'productsCategories': productsCategories,
+        'productsCategories': products_categories,
         'products': products_paginator
     }
 
-    return render(request, 'mainapp/products.html', context)
+    if request.is_ajax():
+        result = render_to_string('mainapp/includes/card.html', context)
+        return JsonResponse({'result': result})
+    else:
+        return render(request, 'mainapp/products.html', context)
 
 
 def item(request, id):
@@ -46,35 +59,3 @@ def item(request, id):
     }
 
     return render(request, 'mainapp/item.html', context)
-
-
-# def category(request, id):
-#     productsCategories = ProductCategory.objects.all()
-#     products = Product.objects.filter(category=id)
-#     context = {
-#         'title': 'GeekShop | Категория',
-#         'productsCategories': productsCategories,
-#         'products': products
-#     }
-#
-#     # for cat in productsCategories:
-#         # print(translit(cat, 'ru', reversed=True))
-#         # print(type(cat.name))
-#
-#     return render(request, 'mainapp/products.html', context)
-
-# TODO:
-# перенести в products
-def category(request, id):
-    if request.is_ajax():
-        productsCategories = ProductCategory.objects.all()
-        products = Product.objects.filter(category=id)
-        context = {
-            'title': 'GeekShop | Категория',
-            'productsCategories': productsCategories,
-            'products': products
-        }
-
-        result = render_to_string('mainapp/includes/card.html', context)
-
-        return JsonResponse({'result': result})
