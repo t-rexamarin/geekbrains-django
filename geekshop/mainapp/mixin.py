dba1f8bdf196173c208687e27a8999177695a810
Roma <65kgm9ca@gmail.com>
2021-12-12 18:57:53 +0300

    mixins small fixes

--- geekshop/mainapp/mixin.py
+++ geekshop/mainapp/mixin.py
@@ -9,16 +9,16 @@ class CustomDispatchMixin(View):
         return super(CustomDispatchMixin, self).dispatch(request, *args, **kwargs)
 
 
-class BaseClassContentMixin(ContextMixin):
+class BaseClassContextMixin(ContextMixin):
     title = ''
 
     def get_context_data(self, **kwargs):
-        context = super(BaseClassContentMixin, self).get_context_data(**kwargs)
+        context = super(BaseClassContextMixin, self).get_context_data(**kwargs)
         context['title'] = self.title
         return context
 
 
 class UserDispatchMixin(View):
-    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    @method_decorator(user_passes_test(lambda u: u.is_authenticated))
     def dispatch(self, request, *args, **kwargs):
         return super(UserDispatchMixin, self).dispatch(request, *args, **kwargs)
