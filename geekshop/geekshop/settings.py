d0f30b105bcae437f219195c0344965c750a995e
Roma <65kgm9ca@gmail.com>
2021-12-28 17:40:10 +0300

    settings hosts fix

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -28,7 +28,7 @@ load_dotenv(BASE_DIR / '.env')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
