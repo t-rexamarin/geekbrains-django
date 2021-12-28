6217ca53fba8e37d4c5866f063d09e0e48c07d1f
Roma <65kgm9ca@gmail.com>
2021-12-28 17:20:50 +0300

    model fix

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class ProductCategory(models.Model):
-    name = models.CharField(verbose_name='имя', max_length=128, unique=True)
+    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
     description = models.TextField(verbose_name='описание', blank=True)
     created_at = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='дата изменения', auto_now=True)
@@ -20,7 +20,7 @@ class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     name = models.CharField(verbose_name='имя продукта', max_length=128)
     image = models.ImageField(upload_to='product_image', blank=True)
-    description = models.CharField(verbose_name='краткое описание продукта', max_length=60, blank=True)
+    description = models.CharField(verbose_name='краткое описание продукта', max_length=128, blank=True)
     price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
     created_at = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
