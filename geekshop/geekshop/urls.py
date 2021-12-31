5340b514a2766ebf13d6b453a881c61868051774
Roma <65kgm9ca@gmail.com>
2021-12-31 14:53:40 +0300

    1) requirements update 2) url debug update

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from mainapp.views import index
 from django.conf import settings
 from django.conf.urls.static import static
@@ -33,4 +33,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
